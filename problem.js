73fd7b50c3f4ec0ecdca3e12159ac5a0c64c9934
Devendra Sharma <146749164+Devendra2005Sharma@users.noreply.github.com>
2026-05-02 15:51:44 +0530

    Add files via upload

---
+++ problem.js
@@ -0,0 +1,19 @@
+// Authentication System(Student portal)
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+
+app.use(express.json());
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = { username: "user@gmail.com", password: "12345..." };
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+        res.send("Login successful");
+    }else {
+        res.send("Invalid ");
+    }
+});
+
+app.listen(3000);
